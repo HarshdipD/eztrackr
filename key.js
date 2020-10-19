d815cb7f743cef08e3c80fc36b850987286756ae
Bhavesh Kumar <bhaveshkumar19856@gmail.com>
2020-10-19 11:38:10 +0530

    var to const

--- key.js
+++ key.js
@@ -1,2 +1,2 @@
-var APP_KEY = 'b891af3a0d98ce8944cb61daf088bc8c';
-var AnalyticsCode = 'Your Analytics Code - UA-XXXX-X'
+const APP_KEY = 'b891af3a0d98ce8944cb61daf088bc8c';
+const AnalyticsCode = 'Your Analytics Code - UA-XXXX-X'
